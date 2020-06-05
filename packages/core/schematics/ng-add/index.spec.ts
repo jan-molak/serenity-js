10bbd3d598683a33d8babe4d5baad683e7648cf9
Christoph Murczek <christoph.murczek@iteratec.com>
2020-06-05 23:33:31 +0200

    feat(schematics): adds schematics to install and scaffold initial files
    this schematic provides setup of serenity with protractor/cucumber. it installs all required
    dependencies, configures protractor and scaffolds the code files for actors and cucumber timeouts.
    the schematic accepts parameters for selecting the testingFramework (currently only cucumber),
    browserIntegration (currently only protractor) as well as the (angular) project name to which
    serenity should be added (defaults to the project set as defaultProject)
    and a directory to use for the e2e testing code (defaults to features).
    
     #359

---
+++ packages/core/schematics/ng-add/index.spec.ts
@@ -0,0 +1,83 @@
+// tslint:disable: no-implicit-dependencies
+import { experimental } from '@angular-devkit/core';
+// tslint:disable-next-line: no-submodule-imports
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+import { expect } from 'chai';
+// tslint:enable: no-implicit-dependencies
+
+// tslint:disable: no-unused-expression
+describe('Angular CLI Schematic', () => {
+    const testRunner = new SchematicTestRunner('@serenity-js/core', require.resolve('../../schematics/collection.json'));
+    let appTree: UnitTestTree
+
+    beforeEach(async () => {
+        const workspaceOptions = { name: 'testspace', newProjectRoot: 'projects', version: '8.0.0' };
+
+        const workspaceTree = await testRunner.runExternalSchematicAsync('@schematics/angular', 'workspace', workspaceOptions).toPromise();
+        appTree = await testRunner.runExternalSchematicAsync('@schematics/angular', 'application', { name: 'defaultApp' }, workspaceTree).toPromise();
+        appTree = await testRunner.runExternalSchematicAsync('@schematics/angular', 'application', { name: 'testApp' }, workspaceTree).toPromise();
+    });
+
+    it('adds the core serenity libraries as devDeps to an angular app', async () => {
+        const tree = await testRunner.runSchematicAsync('ng-add', {}, appTree).toPromise();
+        const packageJson = JSON.parse(tree.readContent('./package.json'));
+        expect(packageJson.devDependencies['@serenity-js/assertions']).to.exist;
+        expect(packageJson.devDependencies['@serenity-js/console-reporter']).to.exist;
+        expect(packageJson.devDependencies['@serenity-js/core']).to.exist;
+        expect(packageJson.devDependencies['@serenity-js/serenity-bdd']).to.exist;
+        expect(packageJson.devDependencies['ts-node']).to.exist;
+    });
+
+    it('scaffolds the creation of actors in the default project', async () => {
+        const tree = await testRunner.runSchematicAsync('ng-add', {}, appTree).toPromise();
+        expect(tree.exists('projects/defaultApp/feature/screenplay/actors.ts')).to.be.true;
+    });
+
+    it('adds the dev dependencies for protractor to the default project by default', async () => {
+        const tree = await testRunner.runSchematicAsync('ng-add', {}, appTree).toPromise();
+        const packageJson = JSON.parse(tree.readContent('package.json'));
+        expect(packageJson.devDependencies['@serenity-js/protractor']).to.exist;
+        expect(packageJson.devDependencies.protractor).to.exist;
+    });
+
+    it('adds the dev dependencies for cucumber to the default project by default', async () => {
+        const tree = await testRunner.runSchematicAsync('ng-add', {}, appTree).toPromise();
+        const packageJson = JSON.parse(tree.readContent('package.json'));
+        expect(packageJson.devDependencies['@serenity-js/cucumber']).to.exist;
+        expect(packageJson.devDependencies['@types/cucumber']).to.exist;
+        expect(packageJson.devDependencies.cucumber).to.exist;
+    });
+
+    it('adds the protractor config file to the e2e test config of the default project', async () => {
+        const tree = await testRunner.runSchematicAsync('ng-add', {}, appTree).toPromise();
+        const ptrConf = tree.readContent('projects/defaultApp/feature/protractor.conf.js');
+        expect(ptrConf.includes('serenity: ')).to.be.true;
+    });
+
+    it('adds the protractor config file to the e2e test config of the specified project', async () => {
+        const options = { project: 'testApp' };
+        const tree = await testRunner.runSchematicAsync('ng-add', options, appTree).toPromise();
+        const ptrConf = tree.readContent('projects/testApp/feature/protractor.conf.js');
+        expect(ptrConf.includes('serenity: ')).to.be.true;
+    });
+
+    it('adds the cucumber configuration to the protractor config in the default project', async () => {
+        const tree = await testRunner.runSchematicAsync('ng-add', {}, appTree).toPromise();
+        const ptrConf = tree.readContent('projects/defaultApp/feature/protractor.conf.js');
+        expect(ptrConf.includes('cucumberOpts: ')).to.be.true;
+    });
+
+    it('scaffolds the configuration of cucumber timeouts in the default project', async () => {
+        const tree = await testRunner.runSchematicAsync('ng-add', {}, appTree).toPromise();
+        expect(tree.exists('projects/defaultApp/feature/support/setup.ts')).to.be.true;
+    });
+
+    it('updates the e2e directory in the angular workspace', async () => {
+        const tree = await testRunner.runSchematicAsync('ng-add', {}, appTree).toPromise();
+        const workspace: experimental.workspace.WorkspaceSchema = JSON.parse(tree.readContent('./angular.json'));
+        expect(workspace.projects.defaultApp.architect!.lint.options.tsConfig).not.to.include('projects/defaultApp/e2e/tsconfig.json');
+        expect(workspace.projects.defaultApp.architect!.lint.options.tsConfig).to.include('projects/defaultApp/feature/tsconfig.json');
+        expect(workspace.projects.defaultApp.architect!.e2e.options.protractorConfig.startsWith('projects/defaultApp/feature')).to.be.true;
+    });
+});
+// tslint:enable: no-unused-expression
